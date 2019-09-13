c2dd5820403e85c5202d13c8ad7bf1ed59370832
jpcanto <jplopessk8@gmail.com>
2019-09-13 01:59:14 -0300

    color picker function

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import { ChromePicker } from "react-color";
 
 export default class Form extends React.Component {
+
+    state = {
+        displayColorPicker: false,
+        defaultColor: "#191919",
+        changeColor: "#191919",
+        color: {
+            r: "0",
+            g: "9",
+            b: "153",
+            a: "1"
+        }
+    };
+
+    onHandleShowColorPicker = () => {
+        this.setState({ displayColorPicker: true });
+    };
+
+    onHandleCloseColorPicker = () => {
+        this.setState({ displayColorPicker: false });
+    };
+
+    onChangeColorPicker = color => {
+        this.setState({ color: color.rgb, changeColor: color.hex });
+    };
+
     render() {
         return (
             <section className="formulario">
@@ -15,7 +41,19 @@ export default class Form extends React.Component {
                 <h2>ICON'S BACKGROUND COLOR</h2>
                 <div className="formulario__cor">
                     <p>Pick a color</p>
-                    <button className="formulario__cor__select-color">#191919</button>
+                    <button className="formulario__cor__select-color" style={{background: this.state.changeColor}} onClick={() => this.onHandleShowColorPicker()}>{this.state.changeColor}</button>
+                    {this.state.displayColorPicker && (
+                        <div className={"formulario__cor-caixa"}>
+                            <div
+                                className={"formulario__cor-caixa--fecha"}
+                                onClick={() => this.onHandleCloseColorPicker()}
+                            />
+                            <ChromePicker
+                                color={this.state.color}
+                                onChange={this.onChangeColorPicker}
+                            />
+                        </div>
+                    )}
                 </div>
                 <h2>CATEGORY</h2>
                 <div className="formulario__categoria">
