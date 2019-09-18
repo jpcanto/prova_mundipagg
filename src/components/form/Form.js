dd3db5c22f9522e66a646dd27bcf73c9f0b6a6a9
jpcanto <jplopessk8@gmail.com>
2019-09-18 00:59:49 -0300

    validação de campos

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -45,7 +45,7 @@ export default class Form extends React.Component {
     };
 
     salvaAppInfo = () => {
-        if (this.state.appName === 'App Name') {
+        if (this.state.appName === 'App Name' || this.state.appName === '') {
             alert('É necessário escolher algum nome');
         } else if (!this.state.files) {
             alert('É necessário escolher alguma imagem');
