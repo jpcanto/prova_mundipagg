577085c75df85548cd40732e78cab191ef2b8105
jpcanto <jplopessk8@gmail.com>
2019-09-20 00:34:52 -0300

    Cobertura de teste para color picker funções

--- src/components/form/Form.test.jsx
+++ src/components/form/Form.test.jsx
@@ -17,3 +17,33 @@ describe('Testando renderização do componente Form', () => {
 
     });
 });
+
+describe('Testando chamada do color picker', () => {
+    it("Deve abrir o color picker ao clicar no botão", () => {
+
+        const wrapper = mount(
+            <Form />
+        );
+
+        expect(wrapper.state('visualizaColorPicker')).toBe(false);
+        wrapper.find('.formulario__cor__select-color').simulate('click');
+        expect(wrapper.state('visualizaColorPicker')).toBe(true)
+
+    });
+
+    it("Deve fechar o color picker ao clicar fora da caixa", () => {
+
+        const wrapper = mount(
+            <Form />
+        );
+
+        wrapper.setState({
+            visualizaColorPicker: true
+        })
+
+        expect(wrapper.state('visualizaColorPicker')).toBe(true);
+        wrapper.find('.formulario__cor-caixa--fecha').simulate('click');
+        expect(wrapper.state('visualizaColorPicker')).toBe(false)
+
+    });
+});
